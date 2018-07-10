b27bc1066ce23e312e261d6fbe46ea924e6d3f5c
jack <steve.a.shogren@gmail.com>
2018-07-10 11:45:35 -0400

    Update to use env for the url

--- src/jira_worklog/creds.clj
+++ src/jira_worklog/creds.clj
@@ -4,9 +4,7 @@
             )
   )
 
-;;(def db-name (env :couch_db_url))
-
-(def db-name "jira-creds")
+(def db-name (env :couchdburl))
 
 (defn get-creds [name]
   (or (:up (c/get-document db-name name))
@@ -14,8 +12,9 @@
 
 (comment
   (c/bulk-update db-name [
-                          { :_id "creque" :up "test"}
+                          { :_id "jimham" :up "test"}
                           ]))
+
 
 ;; (c/put-document db-name (assoc (c/get-document db-name "shogren1") :name "horse"))
 
