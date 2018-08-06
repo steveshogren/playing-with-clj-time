a3866bdd9b105a38cf3b192616d8faeb6dd34dea
jack <steve.a.shogren@gmail.com>
2018-08-06 06:37:35 -0400

    Sorting out pretty printing

--- src/jira_worklog/profiles.clj
+++ src/jira_worklog/profiles.clj
@@ -2,7 +2,8 @@
 
 (defn overwrite-dates [dates]
   (let [contents (read-string (slurp "data.clj"))
-        contents (assoc contents :date-override dates)]
-    contents))
+        contents (assoc contents :date-override dates
+                                 :use-date-override true)]
+    (clojure.pprint/pprint contents (clojure.java.io/writer "data_out.clj"))))
 
 ;; (overwrite-dates ["test", "that"])
